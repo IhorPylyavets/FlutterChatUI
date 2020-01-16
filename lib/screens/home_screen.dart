9f71b47a8e405c3130ac798498e2b9998c7d1c60
Ihor Pylyavets <ihor.pylyavets@gmail.com>
2020-01-16 17:43:56 +0200

    Added appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,30 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () => print('Clicked menu icon'),
+        ),
+        title: Text('Chats',
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () => print('Clicked search icon'),
+          )
+        ],
+      ),
+      body: Text('hello'),
     );
   }
 }
